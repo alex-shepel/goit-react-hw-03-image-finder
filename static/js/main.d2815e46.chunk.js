42b4d61f2be13929a7a145ea5b8eccf9d13b4378
Alex Shepel <iam.alex.shepel@gmail.com>
2021-12-13 13:46:45 +0200

    Updates

--- static/js/main.d2815e46.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var i=n(1),c=n(3),r=n.n(c),o=(n(8),n(0)),s=function(){return Object(o.jsx)("div",{children:"App"})};r.a.render(Object(o.jsx)(i.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.d2815e46.chunk.js.map
